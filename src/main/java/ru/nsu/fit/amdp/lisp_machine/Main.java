89ccc7ff53409dd4538d0250615bd5d04a2cc6e0
Anton Mokrousov <settingx@mail.ru>
2021-11-20 21:26:29 +0700

    Include JavaCC + trivial grammar for Lisp

--- src/main/java/ru/nsu/fit/amdp/lisp_machine/Main.java
+++ src/main/java/ru/nsu/fit/amdp/lisp_machine/Main.java
@@ -1,7 +1,16 @@
 package ru.nsu.fit.amdp.lisp_machine;
 
+import ru.nsu.fit.amdp.lisp_machine.grammar.LispStatement;
+import ru.nsu.fit.amdp.lisp_machine.grammar.ParseException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("It's lisp machine");
+        LispStatement parser = new LispStatement(System.in);
+        try {
+            parser.getLispExpressions();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
