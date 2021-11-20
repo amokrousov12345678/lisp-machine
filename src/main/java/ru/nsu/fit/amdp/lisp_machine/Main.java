46da6801f2edfcb7daed4366f502ef18b29ec259
Anton Mokrousov <settingx@mail.ru>
2021-11-20 23:32:45 +0700

    Complete grammar for parsing lisp

--- src/main/java/ru/nsu/fit/amdp/lisp_machine/Main.java
+++ src/main/java/ru/nsu/fit/amdp/lisp_machine/Main.java
@@ -2,13 +2,15 @@ package ru.nsu.fit.amdp.lisp_machine;
 
 import ru.nsu.fit.amdp.lisp_machine.grammar.LispStatement;
 import ru.nsu.fit.amdp.lisp_machine.grammar.ParseException;
+import ru.nsu.fit.amdp.lisp_machine.grammar.SimpleNode;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("It's lisp machine");
         LispStatement parser = new LispStatement(System.in);
         try {
-            parser.getLispExpressions();
+            SimpleNode root = parser.LispExpressions();
+            root.dump("");
         } catch (ParseException e) {
             e.printStackTrace();
         }
