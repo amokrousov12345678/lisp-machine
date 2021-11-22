aab0c53d977f9f640f66e8eb37f94e666991fbf2
Denis Parfenov <tempoden@yandex.ru>
2021-11-22 21:15:23 +0700

    Add initial context implementation

---
+++ src/main/java/ru/nsu/fit/amdp/lisp_machine/runtime/context/LispContext.java
@@ -0,0 +1,33 @@
+package ru.nsu.fit.amdp.lisp_machine.runtime.context;
+
+import ru.nsu.fit.amdp.lisp_machine.parser.LispIdentifier;
+import ru.nsu.fit.amdp.lisp_machine.runtime.expressions.Expression;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class LispContext implements Context{
+
+    private Map<String, Expression> context;
+
+    public LispContext() {
+        context = new HashMap<>();
+    }
+
+    @Override
+    public Optional<Expression> getByName(LispIdentifier name) {
+        Expression expression = context.get(name.getName());
+
+        if (expression != null) {
+            return Optional.of(expression);
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public void define(LispIdentifier name, Expression value) {
+        context.put(name.getName(), value);
+    }
+}
