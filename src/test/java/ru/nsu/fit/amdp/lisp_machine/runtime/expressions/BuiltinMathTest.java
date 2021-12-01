f9c1c8b20a79749ca70ee73dd87eb6fe6edf0857
Денис Парфенов <tempoden@yandex.ru>
2021-12-01 18:43:24 +0700

    Add tests for builtin arithmetics

--- src/test/java/ru/nsu/fit/amdp/lisp_machine/runtime/expressions/BuiltinMathTest.java
+++ src/test/java/ru/nsu/fit/amdp/lisp_machine/runtime/expressions/BuiltinMathTest.java
@@ -52,5 +52,103 @@ public class BuiltinMathTest {
         Assertions.assertTrue(((LispObject) result).self() instanceof Float);
         Assertions.assertEquals(result, new LispObject(9.0f));
     }
+
+    @Test
+    public void multInteger_correct() throws ParseException {
+        String expr = "(* 2 3 4)";
+        InputStream is = new ByteArrayInputStream(expr.getBytes());
+        var listExprs = LispParser.parseLispProgram(is);
+
+        var operation = listExprs.get(0);
+        var result = operation.evaluate(getArithmeticsContext());
+
+        Assertions.assertTrue(result instanceof LispObject);
+        Assertions.assertTrue(((LispObject) result).self() instanceof Integer);
+        Assertions.assertEquals(result, new LispObject(24));
+    }
+
+    @Test
+    public void multFloat_correct() throws ParseException {
+        String exprWithFloats = "(* 2 3 4.0)";
+        InputStream is = new ByteArrayInputStream(exprWithFloats.getBytes());
+        var listExprs = LispParser.parseLispProgram(is);
+
+        var operation = listExprs.get(0);
+        var result = operation.evaluate(getArithmeticsContext());
+
+        Assertions.assertTrue(result instanceof LispObject);
+        Assertions.assertTrue(((LispObject) result).self() instanceof Float);
+        Assertions.assertEquals(result, new LispObject(24.0f));
+    }
+
+    @Test
+    public void divInteger_correct() throws ParseException {
+        String expr = "(/ 5 2)";
+        InputStream is = new ByteArrayInputStream(expr.getBytes());
+        var listExprs = LispParser.parseLispProgram(is);
+
+        var operation = listExprs.get(0);
+        var result = operation.evaluate(getArithmeticsContext());
+
+        Assertions.assertTrue(result instanceof LispObject);
+        Assertions.assertTrue(((LispObject) result).self() instanceof Integer);
+        Assertions.assertEquals(result, new LispObject(2));
+    }
+
+    @Test
+    public void divFloat_correct() throws ParseException {
+        String exprWithFloats = "(/ 2 4.0)";
+        InputStream is = new ByteArrayInputStream(exprWithFloats.getBytes());
+        var listExprs = LispParser.parseLispProgram(is);
+
+        var operation = listExprs.get(0);
+        var result = operation.evaluate(getArithmeticsContext());
+
+        Assertions.assertTrue(result instanceof LispObject);
+        Assertions.assertTrue(((LispObject) result).self() instanceof Float);
+        Assertions.assertEquals(result, new LispObject(0.5f));
+    }
+
+    @Test
+    public void minusInteger_correct() throws ParseException {
+        String expr = "(- 5 2)";
+        InputStream is = new ByteArrayInputStream(expr.getBytes());
+        var listExprs = LispParser.parseLispProgram(is);
+
+        var operation = listExprs.get(0);
+        var result = operation.evaluate(getArithmeticsContext());
+
+        Assertions.assertTrue(result instanceof LispObject);
+        Assertions.assertTrue(((LispObject) result).self() instanceof Integer);
+        Assertions.assertEquals(result, new LispObject(3));
+    }
+
+    @Test
+    public void minusFloat_correct() throws ParseException {
+        String exprWithFloats = "(- 2 4.0)";
+        InputStream is = new ByteArrayInputStream(exprWithFloats.getBytes());
+        var listExprs = LispParser.parseLispProgram(is);
+
+        var operation = listExprs.get(0);
+        var result = operation.evaluate(getArithmeticsContext());
+
+        Assertions.assertTrue(result instanceof LispObject);
+        Assertions.assertTrue(((LispObject) result).self() instanceof Float);
+        Assertions.assertEquals(result, new LispObject(-2.0f));
+    }
+
+    @Test
+    public void minusUnary_correct() throws ParseException {
+        String exprWithFloats = "(- 2)";
+        InputStream is = new ByteArrayInputStream(exprWithFloats.getBytes());
+        var listExprs = LispParser.parseLispProgram(is);
+
+        var operation = listExprs.get(0);
+        var result = operation.evaluate(getArithmeticsContext());
+
+        Assertions.assertTrue(result instanceof LispObject);
+        Assertions.assertTrue(((LispObject) result).self() instanceof Integer);
+        Assertions.assertEquals(result, new LispObject(-2));
+    }
 }
 
