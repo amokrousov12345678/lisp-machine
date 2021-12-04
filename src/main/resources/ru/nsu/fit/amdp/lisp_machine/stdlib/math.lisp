fe541c0837784688326f1d4519afe213df2be661
Denis Parfenov <tempoden@yandex.ru>
2021-12-05 03:15:47 +0700

    Add increment/decrement

--- src/main/resources/ru/nsu/fit/amdp/lisp_machine/stdlib/math.lisp
+++ src/main/resources/ru/nsu/fit/amdp/lisp_machine/stdlib/math.lisp
@@ -5,3 +5,9 @@
 
 (comment "Check whether provided number is odd")
 (def odd? (fn (n) (= (mod n 2) 1)))
+
+(comment "Returns value increased by 1")
+(def inc (fn (n) (+ n 1)))
+
+(comment "Returns value decreased by 1")
+(def dec (fn (n) (- n 1)))
