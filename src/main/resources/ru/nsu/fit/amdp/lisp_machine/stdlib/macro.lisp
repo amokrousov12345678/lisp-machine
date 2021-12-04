d82eb19a6a18836b66d58190a81101d59efbd4a6
Denis Parfenov <tempoden@yandex.ru>
2021-12-05 01:00:12 +0700

    Add comments and applies

--- src/main/resources/ru/nsu/fit/amdp/lisp_machine/stdlib/macro.lisp
+++ src/main/resources/ru/nsu/fit/amdp/lisp_machine/stdlib/macro.lisp
@@ -1,1 +1,9 @@
 (def infix (macro (fn (first op next) (list op first next))))
+
+(def comment (macro (fn (&args) nil)))
+
+(comment "apply for single list of args")
+(def apply-single (macro (fn (fun arglist) (concat (list fun) (eval arglist)))))
+
+(comment "apply for multiple lists of arguments")
+(def apply (macro (fn (fun &args) (concat (list fun) (apply-single concat args)))))
