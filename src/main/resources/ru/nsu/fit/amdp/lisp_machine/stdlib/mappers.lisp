5b273c767b3a7f6f9438430b89043600a8505ac7
Denis Parfenov <tempoden@yandex.ru>
2021-12-05 23:09:02 +0700

    Add filter

--- src/main/resources/ru/nsu/fit/amdp/lisp_machine/stdlib/mappers.lisp
+++ src/main/resources/ru/nsu/fit/amdp/lisp_machine/stdlib/mappers.lisp
@@ -1,9 +1,26 @@
 (def comment (macro (fn (&args) nil)))
 
+(comment "Returns a new list where x is the last element")
+(def conj (fn (x seq) (concat seq (list x))))
+
 (comment "Map. Usage (map [function elem->f(elem)] [sequence])")
 (def map (fn (fun args)
     (reduce
         (fn (acc e) (conj (fun e) acc))
+        args
+        (list)
+    )
+))
+
+(comment "Filter. Usage (filter [predicate elem->bool] [sequence])")
+(def filter (fn (pred args)
+    (reduce
+        (fn (acc e)
+            (if (pred e)
+                (conj e acc)
+                acc
+            )
+        )
         args
         (list)
     )
