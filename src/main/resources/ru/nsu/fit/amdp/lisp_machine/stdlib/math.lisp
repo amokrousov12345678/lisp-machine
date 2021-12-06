8710bdc6c750589761618a01bdb2b4b361055829
Denis Parfenov <tempoden@yandex.ru>
2021-12-06 23:05:51 +0700

    Add some more functions to math

--- src/main/resources/ru/nsu/fit/amdp/lisp_machine/stdlib/math.lisp
+++ src/main/resources/ru/nsu/fit/amdp/lisp_machine/stdlib/math.lisp
@@ -11,3 +11,20 @@
 
 (comment "Returns value decreased by 1")
 (def dec (fn (n) (- n 1)))
+
+(comment "Returns square of n")
+(def sqr (fn (n) (* n n)))
+
+(comment "Check whether a is divisor of b")
+(def divides? (fn (a b) (= (mod b a) 0)))
+
+(comment "GCD of a b")
+(def gcd (fn (a b)
+    (if (> b a)
+        (recur b a)
+        (if (= b 0)
+            a
+            (recur b (mod a b))
+        )
+    )
+))
