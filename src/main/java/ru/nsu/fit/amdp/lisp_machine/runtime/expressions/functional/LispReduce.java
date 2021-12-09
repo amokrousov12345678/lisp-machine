5ce3b222a438e2fbe7277b70214f3526ee2b665e
Денис Парфенов <tempoden@yandex.ru>
2021-12-10 00:30:29 +0700

    Make reduce lazy

--- src/main/java/ru/nsu/fit/amdp/lisp_machine/runtime/expressions/functional/LispReduce.java
+++ src/main/java/ru/nsu/fit/amdp/lisp_machine/runtime/expressions/functional/LispReduce.java
@@ -2,6 +2,7 @@ package ru.nsu.fit.amdp.lisp_machine.runtime.expressions.functional;
 
 import ru.nsu.fit.amdp.lisp_machine.runtime.context.Context;
 import ru.nsu.fit.amdp.lisp_machine.runtime.expressions.Expression;
+import ru.nsu.fit.amdp.lisp_machine.runtime.expressions.datatypes.ISeq;
 import ru.nsu.fit.amdp.lisp_machine.runtime.expressions.lang.LispExecutableList;
 import ru.nsu.fit.amdp.lisp_machine.runtime.expressions.lang.LispQuotedExpression;
 
@@ -19,14 +20,22 @@ public class LispReduce implements Expression {
         Expression arg = args.get(1).evaluate(context);
         Expression initialAcc = args.get(2).evaluate(context);
 
-        if (!(arg instanceof LispExecutableList)) {
-           throw new RuntimeException("Attempt to apply reduce to non-list");
+        if (!(arg instanceof ISeq)) {
+           throw new RuntimeException("Attempt to apply reduce to non-seq");
         }
 
-        return ((LispExecutableList) arg).asList().stream()
-                .reduce(initialAcc,
-                        // Additional quote added in order to prevent argument evaluation in classes
-                        // derived from LispBaseFunction
-                        (acc, e) -> function.apply(context, List.of(new LispQuotedExpression(acc), e)));
+        Expression acc = initialAcc;
+        ISeq seq = (ISeq) arg;
+        Expression first = seq.first();
+
+        while (first != null) {
+            acc = function.apply(context, List.of(new LispQuotedExpression(acc), first));
+            seq = seq.next();
+            if (seq == null)
+                break;
+            first = seq.first();
+        }
+
+        return acc;
     }
 }
