9a729983cb4c7a59707307f3fb3934a083e1e218
Денис Парфенов <tempoden@yandex.ru>
2021-12-09 21:54:45 +0700

    Add ConcatResult for LazyConcat

---
+++ src/main/java/ru/nsu/fit/amdp/lisp_machine/runtime/expressions/datatypes/ConcatResult.java
@@ -0,0 +1,69 @@
+package ru.nsu.fit.amdp.lisp_machine.runtime.expressions.datatypes;
+
+import ru.nsu.fit.amdp.lisp_machine.runtime.expressions.Expression;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class ConcatResult implements Expression, ISeq {
+
+    private final List<ISeq> sequences;
+    private Expression first = null;
+    private ConcatResult next = null;
+
+    public ConcatResult (List<ISeq> sequences) {
+        this.sequences = sequences;
+    }
+
+    @Override
+    public Expression first() {
+        if (first != null) {
+            return first;
+        }
+
+        for (var seq : sequences) {
+            first = seq.first();
+            if (first != null)
+                break;
+        }
+
+        return null;
+    }
+
+    @Override
+    public ISeq next() {
+        if (next != null)
+            return next;
+
+        List<ISeq> sequencesCopy = new LinkedList<>(sequences);
+        ISeq firstAliveSeq = null;
+        for (var seq : sequencesCopy) {
+            firstAliveSeq = seq.next();
+            sequencesCopy.remove(0);
+            if (firstAliveSeq != null)
+                break;
+        }
+        sequencesCopy.add(0, firstAliveSeq);
+        next = new ConcatResult(sequencesCopy);
+        return next;
+    }
+
+    @Override
+    public String toString(){
+        List<Expression> expressions = new LinkedList<>();
+        for (var seq : sequences) {
+            Expression expr = seq.first();
+            while (expr != null) {
+                expressions.add(expr);
+                seq = seq.next();
+                expr = seq == null ? null : seq.first();
+            }
+        }
+        return expressions.stream()
+                .map(Objects::toString)
+                .collect(Collectors.joining(" ", "(", ")"));
+    }
+
+}
