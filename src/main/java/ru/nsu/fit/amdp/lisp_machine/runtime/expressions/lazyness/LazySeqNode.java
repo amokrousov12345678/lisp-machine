29e3656fef6dc4864f95ef44f203b54e6b21fd4c
Денис Парфенов <tempoden@yandex.ru>
2021-12-10 18:41:10 +0700

    Add toString for lazy-seq result

--- src/main/java/ru/nsu/fit/amdp/lisp_machine/runtime/expressions/lazyness/LazySeqNode.java
+++ src/main/java/ru/nsu/fit/amdp/lisp_machine/runtime/expressions/lazyness/LazySeqNode.java
@@ -3,7 +3,11 @@ package ru.nsu.fit.amdp.lisp_machine.runtime.expressions.lazyness;
 import ru.nsu.fit.amdp.lisp_machine.runtime.context.Context;
 import ru.nsu.fit.amdp.lisp_machine.runtime.expressions.Expression;
 import ru.nsu.fit.amdp.lisp_machine.runtime.expressions.datatypes.ISeq;
-import ru.nsu.fit.amdp.lisp_machine.runtime.expressions.lang.LispExecutableList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class LazySeqNode implements Expression, ISeq {
 
@@ -48,5 +52,23 @@ public class LazySeqNode implements Expression, ISeq {
         execute();
 
         return next;
+    }
+
+    @Override
+    public String toString() {
+        List<Expression> expressions = new ArrayList<>();
+
+        ISeq seq = this;
+        Expression expr = this.first();
+
+        while (expr != null) {
+            expressions.add(expr);
+            seq = seq.next();
+            expr = seq == null ? null : seq.first();
+        }
+
+        return expressions.stream()
+                .map(Objects::toString)
+                .collect(Collectors.joining(" ", "(", ")"));
     }
 }
