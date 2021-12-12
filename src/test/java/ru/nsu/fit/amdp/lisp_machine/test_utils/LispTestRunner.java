5fa84b442e4886912f6fec6ff0aefbf10b6c136d
Denis Parfenov <tempoden@yandex.ru>
2021-12-12 16:15:47 +0700

    Change Objects.equals for Assertions.assertEquals

--- src/test/java/ru/nsu/fit/amdp/lisp_machine/test_utils/LispTestRunner.java
+++ src/test/java/ru/nsu/fit/amdp/lisp_machine/test_utils/LispTestRunner.java
@@ -1,5 +1,6 @@
 package ru.nsu.fit.amdp.lisp_machine.test_utils;
 
+import org.junit.jupiter.api.Assertions;
 import ru.nsu.fit.amdp.lisp_machine.grammar.ParseException;
 import ru.nsu.fit.amdp.lisp_machine.runtime.Machine;
 
@@ -10,7 +11,7 @@ public class LispTestRunner {
 
     private static final Machine commonMachine = Machine.getInstanceWithLoadedLibrary();
 
-    public static boolean checkStatementsForEquality(String lispStatement1,
+    public static void checkStatementsForEquality(String lispStatement1,
                                                      String lispStatement2,
                                                      String contextSetup,
                                                      Machine machine) throws IOException, ParseException {
@@ -26,17 +27,17 @@ public class LispTestRunner {
         var statement1 = TestParser.parseLispStatement(lispStatement1).get(0);
         var statement2 = TestParser.parseLispStatement(lispStatement2).get(0);
 
-        return Objects.equals(machine.evaluate(statement1), machine.evaluate(statement2));
+        Assertions.assertEquals(machine.evaluate(statement2), machine.evaluate(statement1));
     }
 
-    public static boolean checkStatementsForEquality(String lispStatement1,
+    public static void checkStatementsForEquality(String lispStatement1,
                                                      String lispStatement2,
                                                      String contextSetup) throws IOException, ParseException {
-        return checkStatementsForEquality(lispStatement1, lispStatement2, contextSetup, null);
+        checkStatementsForEquality(lispStatement1, lispStatement2, contextSetup, null);
     }
 
-    public static boolean checkStatementsForEquality(String lispStatement1,
+    public static void checkStatementsForEquality(String lispStatement1,
                                                      String lispStatement2) throws IOException, ParseException {
-        return checkStatementsForEquality(lispStatement1, lispStatement2, null);
+        checkStatementsForEquality(lispStatement1, lispStatement2, null);
     }
 }
